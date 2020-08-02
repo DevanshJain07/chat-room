916a5ff5e7ae977a45d2bef6837f93acbe3ae200
devansh071999 <devanshsampatjain@gmail.com>
2020-08-02 20:54:34 +0530

    object conversion from string

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express')
 const path=require('path')
 const http=require('http')
 const socketio=require('socket.io')
+const formatMessage=require('./utils/messages')
 
 const app=express()
 const server=http.createServer(app)
@@ -10,23 +11,25 @@ const io=socketio(server)
 //setting static page
 app.use(express.static(path.join(__dirname,'public')))
 
+const botname='chatitout bot'
+
 //run when client connects
 io.on('connection',socket=>{
     console.log("New WebSocket Connected...")
 
     //new user
-    socket.emit('message','Welcome To Chat It Out')
+    socket.emit('message',formatMessage(botname,'Welcome to Chat It Out'))
 
     //Broadcast when a user connects
-    socket.broadcast.emit('message','A user has joined the chat')
+    socket.broadcast.emit('message',formatMessage(botname,'A user has joined the chat'))
 
     //Runs when client disconnects
     socket.on('disconnect',()=>{
-        io.emit('message','A user has left the chat')
+        io.emit('message',formatMessage(botname,'A user has left the chat'))
     })
 
     socket.on('chatmessage',msg=>{
-        io.emit('message',msg)
+        io.emit('message',formatMessage('USER',msg))
     })
 
 })
